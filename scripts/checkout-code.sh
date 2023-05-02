204c68dfcd3ab7e0bba31def17be153a0be2a9c8
Constantin Adam <cmadam@us.ibm.com>
2023-05-02 11:55:43 -0400

    Configure locally cloned org/branch using env vars

--- scripts/checkout-code.sh
+++ scripts/checkout-code.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 CRT_DIR="${PWD}"
+
+STIX_SHIFTER_BRANCH=${STIX_SHIFTER_BRANCH:-develop}
+KESTREL_BRANCH=${KESTREL_BRANCH:-develop_stixshifter_v5}
+KESTREL_ANALYTICS_BRANCH=${KESTREL_ANALYTICS_BRANCH:-release}
+
+STIX_SHIFTER_ORG=${STIX_SHIFTER_ORG:-opencybersecurityalliance}
+KESTREL_ORG=${KESTREL_ORG:-opencybersecurityalliance}
+KESTREL_ANALYTICS_ORG=${KESTREL_ANALYTICS_ORG:-opencybersecurityalliance}
+
 mkdir -p "${HOME}"/huntingtest
 cd "${HOME}"/huntingtest || exit
-git clone git@github.com:opencybersecurityalliance/stix-shifter.git
-git clone -b develop_stixshifter_v5 git@github.com:opencybersecurityalliance/kestrel-lang.git
-git clone git@github.com:opencybersecurityalliance/kestrel-analytics.git
+git clone -b "$STIX_SHIFTER_BRANCH" git@github.com:"$STIX_SHIFTER_ORG"/stix-shifter.git
+git clone -b "$KESTREL_BRANCH" git@github.com:"$KESTREL_ORG"/kestrel-lang.git
+git clone -b "$KESTREL_ANALYTICS_BRANCH" git@github.com:"$KESTREL_ANALYTICS_ORG"/kestrel-analytics.git
 cd "${CRT_DIR}" || exit
