7431c4fc325994fe6970aa1bc5b75b8540613691
Constantin Adam <cmadam@us.ibm.com>
2023-05-08 23:23:46 -0400

    Bug fix for integration test run

--- scripts/prep-bdd-tests.sh
+++ scripts/prep-bdd-tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+BASE_DIR=$1
 pip install behave
 ES_PWD=$(cat "${HOME}"/huntingtest/.es_pwd)
 cp config/kestrel-stixshifter-config.yml "${HOME}"/huntingtest/kestrel-stixshifter-config.yaml
 sed -i'' -e "s/<ES_PWD>/${ES_PWD}/" "${HOME}"/huntingtest/kestrel-stixshifter-config.yaml
 export KESTREL_STIXSHIFTER_CONFIG="${HOME}"/huntingtest/kestrel-stixshifter-config.yaml
 CRT_DIR=${PWD}
-cd "${HOME}"/huntingtest/kestrel-analytics/analytics/domainnamelookup || exit
+cd "${BASE_DIR}"/kestrel-analytics/analytics/domainnamelookup || exit
 sudo docker build -t kestrel-analytics-domainnamelookup .
 cd "${CRT_DIR}" || exit
--- scripts/run-bdd-tests-local.sh
+++ scripts/run-bdd-tests-local.sh
@@ -2,7 +2,7 @@
 CRT_DIR=${PWD}
 # shellcheck source=/dev/null
 source "${HOME}"/huntingtest/huntingtest/bin/activate
-"${CRT_DIR}"/scripts/prep-bdd-tests.sh
+"${CRT_DIR}"/scripts/prep-bdd-tests.sh "${HOME}"/huntingtest
 export KESTREL_STIXSHIFTER_CONFIG="${HOME}"/huntingtest/kestrel-stixshifter-config.yaml
 behave --logging-level CRITICAL
 cd "${CRT_DIR}" || exit
