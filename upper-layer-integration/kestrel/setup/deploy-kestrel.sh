7c6d67d8c507cbd2cdd812166a88778a2cc9d09f
Constantin Adam <cmadam@us.ibm.com>
2023-05-20 13:45:31 -0400

    Refactor end-to-end testing

--- upper-layer-integration/kestrel/setup/deploy-kestrel.sh
+++ upper-layer-integration/kestrel/setup/deploy-kestrel.sh
@@ -5,7 +5,7 @@ ES_PWD=$(cat "${HOME}"/fedsearchtest/.es_pwd)
 cp upper-layer-integration/kestrel/config/kestrel-stixshifter-config.yml "${HOME}"/fedsearchtest/kestrel-stixshifter-config.yaml
 sed -i'' -e "s/<ES_PWD>/${ES_PWD}/" "${HOME}"/fedsearchtest/kestrel-stixshifter-config.yaml
 echo "Checking Kestrel and Elasticsearch deployment ..."
-if kestrel upper-layer-integration/kestrel/huntflows/kestrel-test.hf; then
+if kestrel upper-layer-integration/kestrel/test/huntflows/kestrel-test.hf; then
     echo "Successfully deployed Kestrel with Elasticsearch data store"
 else
     echo "Failed to deploy Kestrel with Elasticsearch data store"
